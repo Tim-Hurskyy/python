7c5473983bec11a0be95b4855fd344f67464acac
Tim-Hurskyy <85262990+Tim-Hurskyy@users.noreply.github.com>
2021-06-07 14:44:17 -0700

    Create flags.py

---
+++ flags.py
@@ -0,0 +1,52 @@
+#US flag
+print ('US Flag')
+line = '*' * 10 + '⬛' * 30 + '\n'
+block = line * 5
+line2 = '⬛' * 40 + '\n'
+block2 = line2 * 6
+flag = block + block2
+print(flag)
+
+
+
+print()
+print('ukraine flag')
+line = 'E' * 40 + '\n'
+block = line * 6  
+line2 = '⬛' * 40 + '\n'
+block2 = line2 * 6 
+flag = block + block2
+print(flag)
+
+
+
+
+
+
+
+
+
+print()
+print ('Russian flag ')
+line = 'E' * 40 + '\n'
+block = line * 4  
+line2 = '⬛' * 40 + '\n'
+block2 = line2 * 4 
+line3 = 'V' * 40 +'\n'
+block3 = line3 * 4
+flag = block + block2 + block3
+print(flag)
+
+
+
+
+
+
+
+
+
+print()
+print('Italian flag')
+line = 'Z' * 13 + 'V' * 13 + 'W' * 13 + '\n'
+block = line * 10
+print(block)
